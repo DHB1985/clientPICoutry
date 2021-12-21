67141fde639ad2a16409e88eeedd26e21060ca0c
DHB1985 <90072728+DHB1985@users.noreply.github.com>
2021-12-21 11:21:27 -0300

    20211221

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-import styles from './LandingPage.module.css'
+import { getCountries } from "../../redux/actions";
+import styles from "./LandingPage.module.css";
 
 const LandingPage = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCountries());
+  }, [dispatch]);
   return (
     <div className={styles.landingPage}>
       <h1>Bienvenidos</h1>
       <Link to="/home">
-        <button className={styles.buttonLanding+ " "+ styles.type3}>Ingresar</button>
+        <button className={styles.buttonLanding + " " + styles.type3}>
+          Ingresar
+        </button>
       </Link>
     </div>
   );
