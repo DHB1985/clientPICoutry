4d65a3c85bc755194ffd9fb71a01d2a98a4e0646
DHB1985 <90072728+DHB1985@users.noreply.github.com>
2021-12-21 11:30:49 -0300

    765432

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import {  useEffect } from "react";
 import { getCountries } from "../../redux/actions";
 import styles from "./LandingPage.module.css";
 
